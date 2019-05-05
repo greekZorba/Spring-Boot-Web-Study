71ede669d582853acebccf374b5850855d2d2a3c
jinhak Kim <smiling.jinhak@gmail.com>
2019-05-05 17:33:11 +0900

    Initial commit

---
+++ src/main/java/com/web/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.web.controller;
+
+import com.web.annotation.SocialUser;
+import com.web.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping(value = "/loginSuccess")
+    public String loginComplete(@SocialUser User user){
+        return "redirect:/board/list";
+    }
+}
